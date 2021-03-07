67f4d5ea121241b58cd9921d265879c245536ad3
HipyCas <hipycas@gmail.com>
2021-03-07 13:26:27 +0100

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,20 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
-fn main() {
-  println!("Guess the number!");
+const LOWEST_NUMBER: u32 = 1;
+const HIGHEST_NUMBER: u32 = 100;
 
-  let secret_number = rand::thread_rng().gen_range(1, 101);
+fn main() {
+  println!("Guess the number! Between {} & {} included", LOWEST_NUMBER, HIGHEST_NUMBER);
+
+  let secret_number = rand::thread_rng().gen_range(LOWEST_NUMBER, HIGHEST_NUMBER + 1);  // First included, second not
 
   // println!("The secret number is: {}", secret_number);
 
-  loop {
-    println!("Please input your guess: ");
+  let mut left_attempts = 7;
+
+  while left_attempts > 0 {
+    println!("[{} attempts left] Please input your guess: ", left_attempts);
 
     let mut guess = String::new();
 
@@ -18,7 +23,10 @@ fn main() {
 
     let guess: u32 = match guess.trim().parse() {
       Ok(num) => num,
-      Err(_) => continue,  // _ Catches all errors, no matter what information they have inside
+      Err(_) => {
+        println!("You did not input a number, please input again");
+        continue;
+      },  // _ Catches all errors, no matter what information they have inside
     };
 
     println!("You guessed: {}", guess);
@@ -31,5 +39,11 @@ fn main() {
       },
       Ordering::Greater => println!("Too big!")
     }
+
+    left_attempts -= 1;
+  }
+
+  if left_attempts <= 0 {
+    println!("You ran out of attempts! The number was: {}", secret_number);
   }
 }
